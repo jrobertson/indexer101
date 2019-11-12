59ac43072dd9cffa8ff12c96d622bebab020a339
James Robertson <james@jamesrobertson.eu>
2019-11-12 17:33:44 +0000

    feature: Dynarex documents can now be easily indexed.

--- indexer101.gemspec
+++ indexer101.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'indexer101'
-  s.version = '0.1.0'
-  s.summary = 'Experimental gem to search a list of words 1 character at a time. Intended for use as auto suggestion.'
+  s.version = '0.2.0'
+  s.summary = 'Experimental gem to search a list of words 1 character at ' + 
+      'a time. Intended for use as auto suggestion.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/indexer101.rb']
-  s.add_runtime_dependency('c32', '~> 0.2', '>=0.2.0')
+  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.21')
   s.signing_key = '../privatekeys/indexer101.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/indexer101.rb
+++ lib/indexer101.rb
@@ -5,24 +5,33 @@
 require 'c32'
 require 'thread'
 require 'thwait'
+require 'dynarex'
 
 
 class Indexer101
   using ColouredText
     
   class Index
-    
+
     attr_reader :h
-    attr_accessor :index    
+    attr_accessor :uri_index, :index    
     
     def initialize()          
+
+      @uri_index = {} # contains each URI long with the title
+      @index = {} # contains eack keyword
+      @h = {} # nested keywords constructed from shared string keys
+
     end
     
     def build(a)
       
       threads = []
-      threads << Thread.new do
-        @index = Hash[a.map(&:to_sym).zip([''] * a.length)]
+      
+      if @index.empty? then
+        threads << Thread.new do
+          @index = Hash[a.map(&:to_sym).zip([''] * a.length)]
+        end
       end
       
       threads << Thread.new { @h = group a }
@@ -70,14 +79,14 @@ class Indexer101
     
   end
 
-  def build(a)
+  def build(a=@indexer.index.keys)
     
     t = Time.now
     @indexer.build(a)    
     t2 = Time.now - t
     
     puts "%d words indexed".info % a.length
-    puts "index built in %.2f seconds".info % t2 
+    puts ("index built in " + ("%.3f" % t2).brown + " seconds").info
     
     self
   end
@@ -97,7 +106,7 @@ class Indexer101
     t2 = Time.now - t
     
     puts "index contains %d words".info % @indexer.index.length
-    puts "index read in %.2f seconds".info % t2 
+    puts "index read in " + ("%.2f" % t2).brown + " seconds".info
     
   end
   
@@ -108,8 +117,45 @@ class Indexer101
     end 
     
   end
+  
+  def scan_dxindex(*locations)
+    
+    t = Time.now
+    threads = locations.flatten.map do |location|
+      Thread.new {Thread.current[:v] = Dynarex.new location}      
+    end
+    
+    ThreadsWait.all_waits(*threads)
+    
+    a = threads.map {|x| x[:v]}
+    t2 = Time.now - t
+    puts ("dxindex documents loaded in " + ("%.2f" % t2).brown \
+          + " seconds").info
+    
+    a.each.with_index do |dx, i|
+      
+      @indexer.uri_index.merge! Hash[dx.all.reverse.map.with_index \
+                      {|x,j| [(i+1)*10000 + (j+1), [x.title, x.url].join(' ')]}]
+              
+      dx.all.reverse.each.with_index do |x,j|
+        x.title.scan(/#(\w+)/).flatten(1).each do |keyword|
+          @indexer.index[keyword.to_sym] ||= []
+          @indexer.index[keyword.to_sym] << (i+1)*10000 + (j+1)
+        end
+      end    
+      
+    end
+    
+  end
+  
+  def uri_index()
+    @indexer.uri_index
+  end
 
-  def search(s, limit: 10)
+  # enter a few starting characters and lookup will suggest a few keywords
+  # useful for an auto suggest feature
+  #
+  def lookup(s, limit: 10)
 
     t = Time.now
     a = scan_path s
@@ -124,7 +170,39 @@ class Indexer101
     
     results = scan_leaves(r).sort_by(&:length).take(limit)
     t2 = Time.now - t
-    puts "search took %.2f seconds" % t2 if @debug
+    puts ("lookup took " + ("%.3f" % t2).brown + " seconds").info
+    
+    return results
+    
+  end
+  
+  # enter the exact keywords to search from the index
+  #
+  def search(*keywords)
+    
+    t = Time.now
+    
+    results = keywords.flatten(1).flat_map do |x|
+      
+      a = []
+      a += @indexer.index[x.to_sym].reverse if @indexer.index.has_key? x.to_sym
+      
+      if x.length > 3 then
+        a += @indexer.index.keys.reverse.grep(/^#{x}/).flat_map\
+            {|y| @indexer.index[y]}
+        a += @indexer.index.keys.reverse.grep(/#{x}/).flat_map\
+            {|y| @indexer.index[y]} 
+      end
+      
+      puts ('a: ' + a.inspect).debug if @debug
+      a.uniq.map {|y| @indexer.uri_index[y].split(/\s+(?=https?[^\s]+$)/,2) }
+      
+    end
+    
+    t2 = Time.now - t
+    puts ("found %s results" % results.length).info
+    puts ("search took " + ("%.3f" % t2).brown + " seconds").info
+    puts
     
     return results
     
